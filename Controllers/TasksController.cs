8403334cea1d140c2565ca85a741c71b88083c4d
allanmdev <86642448+allanmdev@users.noreply.github.com>
2024-11-25 21:08:48 -0300

    init repo

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TodoList.Data;
+using TodoList.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public TasksController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<TaskModel>> Get()
+    {
+        return _context.tasks.ToList();
+    }
+
+    [HttpPost]
+    public IActionResult Post(TaskModel tasks)
+    {
+        if (string.IsNullOrWhiteSpace(tasks.Description) || tasks.Description.Length < 10)
+        {
+            return BadRequest("Description precisa ter mais que 10 caracteres.");
+        }
+
+        _context.tasks.Add(tasks);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(Get), new { id = tasks.Id }, tasks);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var task = _context.tasks.FirstOrDefault(t => t.Id == id);
+        if (task == null)
+        {
+            return NotFound("Tarefa não encontrada.");
+        }
+
+        _context.tasks.Remove(task);
+        _context.SaveChanges();
+        return NoContent();
+    }
+}
